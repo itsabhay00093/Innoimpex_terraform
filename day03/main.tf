6ab9001d64bec72b0d482a57c9dc02cbfe676d0c
itsabhay00093 <itsabhay00093@gmail.com>
2025-07-19 21:51:48 +0530

    added day03 code

---
+++ day03/main.tf
@@ -0,0 +1,120 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+/*
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow-ssh-http"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = "vpc-0ae75acd847e1d688"  # Make sure to pass this variable
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # All protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-ssh-http"
+  }
+}*/
+
+##======
+/*
+resource "aws_security_group" "allow_ingress" {
+  name        = "allow-ingress"
+  description = "Allow custom ingress traffic"
+  vpc_id      = "vpc-0ae75acd847e1d688"
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-ingress"
+  }
+}
+*/
+
+
+locals {
+  ingress_rules = [
+    {
+      description = "Allow SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "Allow HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+resource "aws_security_group" "allow_ingress" {
+  name        = "allow-ingress"
+  description = "Allow custom ingress traffic"
+  vpc_id      = "vpc-0ae75acd847e1d688"
+
+  dynamic "ingress" {
+    for_each = local.ingress_rules
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-ingress"
+  }
+}
+
+
---
+++ day03/terraform.tfvars
@@ -0,0 +1,17 @@
+ingress_rules = [
+  {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  },
+  {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+]
+
