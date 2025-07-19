9da214a1de5a3936488e159b8d935da639cb6f4e
itsabhay00093 <itsabhay00093@gmail.com>
2025-07-19 23:44:19 +0530

    updated variable files

---
+++ day03/01/variables.tf
@@ -0,0 +1,11 @@
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
